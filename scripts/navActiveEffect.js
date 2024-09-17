ba52a765b9cf1659cfcefc7bce44741bcee288d9
EmanuelMunozTorigino <emanuel.munoz.torigino@gmail.com>
2024-09-17 18:14:36 -0300

    Create the neccesary staff like folders, imgs and index.html file, css and js files..

---
+++ scripts/navActiveEffect.js
@@ -0,0 +1,13 @@
+// Select all the links from the DOM object
+const navLinks = document.querySelectorAll(".nav-link");
+
+// Add a new click event for each link..
+navLinks.forEach((link) => {
+  link.addEventListener("click", function () {
+    // Remove the active function for all links
+    navLinks.forEach((item) => item.classList.remove("active"));
+
+    // Just add the active class for the clicked link
+    this.classList.add("active");
+  });
+});
