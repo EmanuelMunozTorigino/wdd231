e591db4113a1666570bdae6ec2314398d98a9d98
EmanuelMunozTorigino <emanuel.munoz.torigino@gmail.com>
2024-09-27 10:48:02 -0300

    change the logo url for the json file

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,6 +1,8 @@
 const gridbtn = document.getElementById("gridButton");
 const listbtn = document.getElementById("listButton");
 
+const companiesContainer = document.getElementById("grid");
+
 const url =
   "https://emanuelmunoztorigino.github.io/wdd231/chamber/data/members.json";
 
@@ -10,23 +12,42 @@ async function getCompanies(url) {
   if (response) {
     companiesList = await response.json();
 
-    console.table(companiesList.companies);
-
     displayCompanies(companiesList.companies);
   }
 }
 
 const displayCompanies = (companiesList) => {
-  const $articleContainer = document.createElement("article");
-  const $sectionContainer = document.createElement("section");
-
-  const $companyLogo = document.createElement("img");
-
-  const $addressPara = document.createElement("P");
-
-  const $phoneNumberPara = document.createElement("P");
-
   const $companyURL = document.createElement("a");
+  companiesList.forEach((company) => {
+    const $sectionContainer = document.createElement("section");
+
+    const $companyName = document.createElement("h2");
+
+    const $companyLogo = document.createElement("img");
+
+    const $addressPara = document.createElement("P");
+
+    const $phoneNumberPara = document.createElement("P");
+
+    const $websiteURL = document.createElement("a");
+
+    $companyName.innerHTML = company.name;
+
+    $companyLogo.setAttribute("src", company.logo);
+
+    $companyLogo.setAttribute("alt", `${company.name} Logo`);
+
+    $websiteURL.innerHTML = company.websiteURL;
+
+    $addressPara.innerHTML = `Address: ${company.address}`;
+    $phoneNumberPara.innerHTML = `Phone: ${company.phoneNumber}`;
+
+    $sectionContainer.appendChild($companyName);
+    $sectionContainer.appendChild($companyLogo);
+    $sectionContainer.appendChild($websiteURL);
+    $sectionContainer.appendChild($addressPara);
+    $sectionContainer.appendChild($phoneNumberPara);
+  });
 };
 
-let companiesList = getCompanies(url);
+getCompanies(url);
