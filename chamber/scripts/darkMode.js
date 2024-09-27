ab52bd6b0f5c44ee4764b1769a4395a381349359
EmanuelMunozTorigino <emanuel.munoz.torigino@gmail.com>
2024-09-27 19:38:45 -0300

    same as before

--- chamber/scripts/darkMode.js
+++ chamber/scripts/darkMode.js
@@ -1,6 +1,11 @@
 const $modeBtn = document.getElementById("darkMode");
 
+const $header = document.querySelector("header");
+const $main = document.querySelector("main");
 const $body = document.body;
+const $footer = document.querySelector("footer");
+
+const $companiesArticle = document.getElementById("grid");
 
 $modeBtn.addEventListener("click", () => {
   if ($modeBtn.alt === "Dark Mode Logo") {
@@ -10,5 +15,12 @@ $modeBtn.addEventListener("click", () => {
     $modeBtn.src = "images/dark.png";
     $modeBtn.alt = "Dark Mode Logo";
   }
+
+  //$main.classList.toggle("dark-mode");
+
   $body.classList.toggle("dark-mode");
+  $header.classList.toggle("dark-mode");
+  $footer.classList.toggle("dark-mode");
+
+  $companiesArticle.classList.toggle("dark-mode");
 });
--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,7 +1,7 @@
 const gridbtn = document.getElementById("gridButton");
 const listbtn = document.getElementById("listButton");
 
-const companiesContainer = document.getElementById("grid");
+const $companiesContainer = document.getElementById("grid");
 
 const url =
   "https://emanuelmunoztorigino.github.io/wdd231/chamber/data/members.json";
@@ -17,7 +17,6 @@ async function getCompanies(url) {
 }
 
 const displayCompanies = (companiesList) => {
-  const $companyURL = document.createElement("a");
   companiesList.forEach((company) => {
     const $sectionContainer = document.createElement("section");
 
@@ -34,9 +33,13 @@ const displayCompanies = (companiesList) => {
     $companyName.innerHTML = company.name;
 
     $companyLogo.setAttribute("src", company.logo);
-
     $companyLogo.setAttribute("alt", `${company.name} Logo`);
+    $companyLogo.setAttribute("loading", "lazy");
+    $companyLogo.setAttribute("width", "200");
 
+    $websiteURL.setAttribute("href", company.websiteURL);
+    $websiteURL.setAttribute("target", "_blank");
+    $websiteURL.setAttribute("rel", "noopener noreferrer");
     $websiteURL.innerHTML = company.websiteURL;
 
     $addressPara.innerHTML = `Address: ${company.address}`;
@@ -44,10 +47,22 @@ const displayCompanies = (companiesList) => {
 
     $sectionContainer.appendChild($companyName);
     $sectionContainer.appendChild($companyLogo);
-    $sectionContainer.appendChild($websiteURL);
     $sectionContainer.appendChild($addressPara);
     $sectionContainer.appendChild($phoneNumberPara);
+    $sectionContainer.appendChild($websiteURL);
+
+    $companiesContainer.appendChild($sectionContainer);
   });
 };
 
 getCompanies(url);
+
+gridbtn.addEventListener("click", () => {
+  $companiesContainer.classList.add("grid");
+  $companiesContainer.classList.remove("list");
+});
+
+listbtn.addEventListener("click", () => {
+  $companiesContainer.classList.add("list");
+  $companiesContainer.classList.remove("grid");
+});
