0f0f78bb363ca40b31e9dc712d69c303bc9e7962
EmanuelMunozTorigino <emanuel.munoz.torigino@gmail.com>
2024-10-23 01:37:36 -0300

    Added some modals with course information to the home page course section..

---
+++ scripts/courseModal.js
@@ -0,0 +1,14 @@
+import { courses } from "../data/coursesData.js";
+
+const courseContainer = document.getElementById("filtered");
+
+
+/*
+const myDialog = document.getElementById("mydialog");
+const dialogTitle = document.querySelector("#mydialog h2");
+const closeButton = document.querySelector("#mydialog button");
+const dialogInfo = document.querySelector("#mydialog p");
+
+closeButton.addEventListener("click", () => myDialog.close());
+
+*/
